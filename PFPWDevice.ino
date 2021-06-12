ae81026958614d2ccc2921cff1e7230310b45b80
Hugo <hugo_1199@hotmail.com>
2021-06-12 18:20:28 -0500

    Module completed

--- PFPWDevice.ino
+++ PFPWDevice.ino
@@ -1,14 +1,31 @@
+
+
+#if defined(ARDUINO_ESP8266_NODEMCU)
+  #include <ESP8266WiFi.h>
+  #include <ESP8266HTTPClient.h>
+  int pin = D2;
+#else
+  #include <HTTPClient.h>
+  int pin = 22;
+#endif
+
+
 #include "DHTesp.h" // Incluyo la librería DHTesp
 DHTesp dht;
 
+unsigned long lastMotorRunTime;
+
+
 const char* ssid = "Luna Inc";
 const char* password = "Ydfmx69VN!74";
-const char* host = "192.168.1.74";
+String urlRequest = "http://192.168.1.79:8080/api/v1/device/save";
+String device_uuid = "PFPW60c485af4b5c8";
+String token = "Bearer BB28zTNUInRYqRjtrTN8gjEtkkvz8eiBJsfs24bb";
 
 const char* statusOK = "OK";
 
-float parametro1;
-float parametro2;
+
+HTTPClient httpClientTemp;
 
 /************************************************************************************************
  ************************************************************************************************/
@@ -16,13 +33,13 @@ float parametro2;
 void setup() {
   Serial.begin(115200); 
   delay(1);
-  dht.setup(22,DHTesp::DHT11);
-  //Serial.print("conectando a ");
-  //Serial.println(ssid);
-  //WiFi.begin(ssid, password);
+  dht.setup(pin,DHTesp::DHT11);
+  Serial.print("conectando a ");
+  Serial.println(ssid);
+  
+   WiFi.begin(ssid, password);
 
-  /*
-   while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
@@ -30,8 +47,9 @@ void setup() {
   Serial.println("WiFi conectado");
   Serial.println("direccion IP: ");
   Serial.println(WiFi.localIP());
-   * 
-   */
+   
+
+ 
 }
 
 /***********************************************************
@@ -39,69 +57,50 @@ void setup() {
  
 void loop() {
 
-  /*
-   Serial.print("conectado a ");
-  Serial.println(host);
 
-  // Use WiFiClient class to create TCP connections
-  WiFiClient client;
-  const int httpPort = 80;
-  if (!client.connect(host, httpPort)) {
-    Serial.println("Fallo conexion");
-    return;
-  }
-   */
-
-
+      Serial.println("GG");
       delay(dht.getMinimumSamplingPeriod());
       float humidity = dht.getHumidity(); // obtenemos la humedad
       float temperature = dht.getTemperature(); //obtenemos la temperatura
 
+      
+
       if(dht.getStatusString() == statusOK){
-        Serial.print("Temperatura: ");
+//3,600,000
+        if(millis()-lastMotorRunTime > 900000)  // 15 minutes have passed
+    {
+          Serial.print("Temperatura: ");
         Serial.print(temperature);
         Serial.print(" Humedad: ");
         Serial.print(humidity);
         Serial.println();
-      }
+
+        //clientTemp.setInsecure(); //the magic line, use with caution
+        //clientTemp.connect(urlRequest, 8080);
+        httpClientTemp.begin(urlRequest);
+        httpClientTemp.addHeader("Content-Type", "application/json");
+        httpClientTemp.addHeader("Authorization", token);
+        String idSensorDevice = "1";
+        String body = "{\"token\":\""+device_uuid+"\",\"values\":[{\"item\":\"Humedad\",\"valor\":\""+humidity+"\"},{\"item\":\"Temperatura\",\"valor\":\""+temperature+"\"}],\"sensor_device_id\": \""+idSensorDevice+"\"}";
+        int httpResponseCode = httpClientTemp.POST(body);
+        if  (httpResponseCode > 0){
+            
+            Serial.print("ResponseCode: ");
+            Serial.println(httpResponseCode);
+           }
+          httpClientTemp.end();        
+         lastMotorRunTime = millis();
+    }
+    delay(100); // loop about every 1/10 second, or whatever works
+        
+       
+       
+       }
+
+      
 
      
 
-/*
-         
-  // We now create a URI for the request
-  String url = "/save_as.php?";
-  url += "temperatura=";
-  url += parametro1;
-  
- url += "&humedad=";
- url += parametro2;
 
-  Serial.print("Buscando URL: ");
-  Serial.println(url);
-
-  //..................................//
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" +
-               "Connection: close\r\n\r\n");
-  unsigned long timeout = millis();
-  while (client.available() == 0) {
-    if (millis() - timeout > 5000) {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      return;
-    }
-  }
-
-  // Read all the lines of the reply from server and print them to Serial
-  while (client.available()) {
-    String line = client.readStringUntil('\r');
-    Serial.print(line);
-  }
-
-  Serial.println();
-  Serial.println("conexiÛn de cierre");
-  delay(10000);
- */
   
 }
